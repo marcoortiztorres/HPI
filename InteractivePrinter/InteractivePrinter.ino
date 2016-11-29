9438481c253c9c3a54281e4734624cebcfd83b43
Marco Ortiz Torres <maor9361@colorado.edu>
2016-11-29 16:33:53 -0700

    Update InteractivePrinter.ino

--- InteractivePrinter/InteractivePrinter.ino
+++ InteractivePrinter/InteractivePrinter.ino
@@ -4,72 +4,42 @@
  *        Date : November 9, 2016
  * Description : The purpose of this program is to use sensor data
  *               that (specifically a button) to create or alter the
- *               gcode for a printer to print out.
+ *               gcode for a printer to print out. The arduino code
+ *               prints the sensor data onto the terminal and the
+ *               ProcessingCode reads what is being written on the
+ *               terminal.
  *   Reference : https://www.arduino.cc/en/Reference/FileWrite
+ *               http://electronics.stackexchange.com/questions/54/saving-arduino-sensor-data-to-a-text-file
+ *               https://learn.sparkfun.com/tutorials/connecting-arduino-to-processing
  ==================================================== */
 #include <SD.h>
 
-// Pin numbers that will stay constant
-const int button1 = 2;    // X
-const int button2 = 3;    // Y
-const int button3 = 4;    // Z
-const int button4 = 5;    // 
-const int button5 = 6;    // 
-const int button6 = 7;    // Export .gcode
-const int ledPin =  13;   // Test LED Pin
+int ButtonPin = A2;
+int LightPin = A3;
+int tempPin = A4;
 
-String fileName = "text.txt";   // NAME OF FILE TO WRITE TO
-
-// variables will change
-int buttonState = 0;      // variable for reading the pushbutton status
+int buttonValue;
+int lightValue;
 
 void setup() 
 {
+  pinMode(ButtonPin, INPUT);
+  pinMode(LightPin, INPUT);
+  pinMode(tempPin, INPUT);
+  
+  pinMode(A5, OUTPUT);
+  digitalWrite(A5, HIGH);
   Serial.begin(9600);
-  Serial.println("Initializing memory");
-  SD.open(fileName);
-  pinMode(ledPin, OUTPUT);    // initialize the LED pin as an output:
-  pinMode(button1, INPUT);    // initialize the pushbutton pin as an input
-  
-  
 }
 
 void loop() 
 { 
-  buttonState = digitalRead(button1);  // checking the state of the button
-  buttonState = digitalRead(button2);
-  buttonState = digitalRead(button3);
-  buttonState = digitalRead(button4);
-  buttonState = digitalRead(button5);
-  buttonState = digitalRead(button6);
+  buttonValue = analogRead(ButtonPin)+ 1;
+  //Serial.println(buttonValue);    // Sends the button value
+  lightValue = analogRead(LightPin);// + 1000; // increased it by 1000 to destinguish that it is the data being read by light sensor
+  tempValue = analogRead(tempPin);
 
-  if (button1 == HIGH)                      // If button 1 is pressed
-  {
-    digitalWrite(ledPin, HIGH);             // Turn the LED on
-  }
-  else if (button2 == HIGH) 
-  {
-    // X
-  }
-  else if (button3 == HIGH) 
-  {
-    // Y
-  }
-  else if (button4 == HIGH) 
-  {
-    // Z
-  }
-  else if (button5 == HIGH) 
-  {
-    
-  }
-  else if (button6 == HIGH) 
-  {
-    SD.close();
-  }
-  else 
-  {
-    digitalWrite(ledPin, LOW);              // turn LED off:
-  }
+   
+  Serial.println(lightValue);     // Sends the sensor data of the light sensor
+  delay(200);
 }
-
